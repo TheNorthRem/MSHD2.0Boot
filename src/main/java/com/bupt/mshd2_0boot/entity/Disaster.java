f214ebe9804d422322530ed81efff7d1688dedd8
Jungle <1239946358@qq.com>
2023-10-24 20:34:23 +0800

    实体类

---
+++ src/main/java/com/bupt/mshd2_0boot/entity/Disaster.java
@@ -0,0 +1,29 @@
+package com.bupt.mshd2_0boot.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+@TableName("disaster")
+@Schema(description = "灾情数据")
+public class Disaster {
+    @TableId(type = IdType.AUTO)
+    @Schema(description = "主键")
+    private Integer disasterId;
+
+    @TableField("id")
+    @Schema(description = "灾情编码")
+    private String id;
+
+    @TableField("description")
+    @Schema(description = "描述")
+    private String description;
+
+    @TableField("file_path")
+    @Schema(description = "载体路径")
+    private String filePath;
+}
---
+++ src/main/java/com/bupt/mshd2_0boot/entity/User.java
@@ -0,0 +1,50 @@
+package com.bupt.mshd2_0boot.entity;
+
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Timestamp;
+
+@Data
+@TableName("user")
+@Schema(description = "用户信息")
+public class User {
+    @TableId(type = IdType.AUTO)
+    @Schema(description = "主键，用户id")
+    private Integer id;
+
+    @TableField("phone")
+    @Schema(description = "电话号码")
+    private String phone;
+
+    @TableField("password")
+    @Schema(description = "密码")
+    private String password;
+
+    @TableField("nick_name")
+    @Schema(description = "用户名")
+    private String nickName;
+
+    @TableField("icon")
+    @Schema(description = "头像")
+    private String icon;
+
+    @TableField("create_time")
+    @Schema(description = "创建时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    private Timestamp createTime;
+
+    @TableField("update_time")
+    @Schema(description = "更新时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    private Timestamp updateTime;
+}
