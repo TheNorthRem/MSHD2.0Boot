81820ae5bef88c3d07f2b8fb6518d9cf676e0944
=TheNorthRem <=1371002466@qq.com>
2023-10-25 11:45:35 +0800

    Utils

---
+++ src/main/java/com/bupt/mshd2_0boot/utils/EncodeUtils.java
@@ -0,0 +1,14 @@
+package com.bupt.mshd2_0boot.utils;
+
+import java.util.Map;
+
+public class EncodeUtils {
+    public static String Encodes(Map<String,String> data){
+
+        return null ;
+    }
+
+    public static  Map<String,String> decodes(String str){
+        return null;
+    }
+}
