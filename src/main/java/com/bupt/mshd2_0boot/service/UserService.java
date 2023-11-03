afa944e041a624dc042a6c2097633a8ccac2661d
Jungle <1239946358@qq.com>
2023-11-03 23:35:19 +0800

    加入token验证功能，用于后续的登出操作

--- src/main/java/com/bupt/mshd2_0boot/service/UserService.java
+++ src/main/java/com/bupt/mshd2_0boot/service/UserService.java
@@ -5,7 +5,31 @@ import com.bupt.mshd2_0boot.entity.User;
 import com.bupt.mshd2_0boot.utils.Result;
 
 public interface UserService extends IService<User> {
+    /**
+     * 登录接口，查询数据库中是否有对应的用户电话号码和密码
+     *
+     * @param phone    电话号码
+     * @param password 密码
+     * @return 登录结果
+     */
     Result login(String phone, String password);
 
+    /**
+     * 注册接口
+     *
+     * @param userName 用户姓名
+     * @param phone    电话号码
+     * @param password 密码
+     * @return 注册结果
+     */
     Result register(String userName, String phone, String password);
+
+    /**
+     * 注销接口
+     *
+     * @param phone    电话号码
+     * @param password 密码
+     * @return 注销结果
+     */
+    Result delete(String phone, String password);
 }
---
+++ src/main/java/com/bupt/mshd2_0boot/utils/RedisConstants.java
@@ -0,0 +1,10 @@
+package com.bupt.mshd2_0boot.utils;
+
+/**
+ * Redis默认数据
+ */
+public class RedisConstants {
+    public final static String LOGIN_USER_KEY = "login:token:";
+
+    public static final Long LOGIN_USER_TTL = 36000L;
+}
