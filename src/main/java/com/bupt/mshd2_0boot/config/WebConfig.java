9f79d2da45d537804c37ee7b9a7ca6c1caf839c8
Jungle <1239946358@qq.com>
2023-11-07 20:15:54 +0800

    跨域请求

---
+++ src/main/java/com/bupt/mshd2_0boot/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.bupt.mshd2_0boot.config;
+
+import jakarta.annotation.Nonnull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域配置，下面的url就是前端对应的url
+ */
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@Nonnull CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
