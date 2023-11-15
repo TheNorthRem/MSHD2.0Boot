bc09f49bb8deba8ae4a6af8039ed5201ab30a017
=TheNorthRem <=1371002466@qq.com>
2023-11-15 11:18:57 +0800

    加注释～

--- src/main/java/com/bupt/mshd2_0boot/controller/DisasterController.java
+++ src/main/java/com/bupt/mshd2_0boot/controller/DisasterController.java
@@ -52,7 +52,7 @@ public class DisasterController {
             decodes.put("description",disaster.getDescription());
             decodes.put("uploadTime",disaster.getUploadTime().toString());
             decodes.put("updateTime",disaster.getUpdateTime().toString());
-            decodes.put("uploader",userService.getById(disaster.getUploader()).toString());
+            decodes.put("uploader",userService.getById(disaster.getUploader()).getUsername());
             res.add(decodes);
         }
         return Result.ok(res);
