f75e2f8ef9a243d699b5ad0dcdd9ec340151b7ed
Jungle <1239946358@qq.com>
2023-11-20 14:07:38 +0800

    更改redis前缀，和BSDN服务器做区分

--- src/main/java/com/bupt/mshd2_0boot/utils/RedisConstants.java
+++ src/main/java/com/bupt/mshd2_0boot/utils/RedisConstants.java
@@ -4,7 +4,7 @@ package com.bupt.mshd2_0boot.utils;
  * Redis默认数据
  */
 public class RedisConstants {
-    public final static String LOGIN_USER_KEY = "login:token:";
+    public final static String LOGIN_USER_KEY = "MSHD2.0:login:token:";
 
     public static final Long LOGIN_USER_TTL = 36000L;
 
