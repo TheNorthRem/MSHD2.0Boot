cba8f2dc7b7a91ad558cdce8be0e25f556886c5e
Jungle <1239946358@qq.com>
2023-11-20 14:09:05 +0800

    更改redis登录有效期改为一天

--- src/main/java/com/bupt/mshd2_0boot/utils/RedisConstants.java
+++ src/main/java/com/bupt/mshd2_0boot/utils/RedisConstants.java
@@ -6,7 +6,7 @@ package com.bupt.mshd2_0boot.utils;
 public class RedisConstants {
     public final static String LOGIN_USER_KEY = "MSHD2.0:login:token:";
 
-    public static final Long LOGIN_USER_TTL = 36000L;
+    public static final Long LOGIN_USER_TTL = 1440L;
 
     public static final String provinces = "MSHD2:provinces";
 }
