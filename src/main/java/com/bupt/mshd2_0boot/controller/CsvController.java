446a0c7c8dd4e3c04e521fb7551ed8078285d16e
=TheNorthRem <=1371002466@qq.com>
2023-12-12 11:59:03 +0800

    分页

--- src/main/java/com/bupt/mshd2_0boot/controller/CsvController.java
+++ src/main/java/com/bupt/mshd2_0boot/controller/CsvController.java
@@ -1,14 +1,37 @@
 package com.bupt.mshd2_0boot.controller;
 
+import com.bupt.mshd2_0boot.entity.Disaster;
+import com.bupt.mshd2_0boot.utils.ParseFileTools;
+import com.bupt.mshd2_0boot.utils.Result;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
-@RequestMapping("/Address")
+@RequestMapping("/Csv")
 @Slf4j
 @Tag(name = "地址")
 public class CsvController {
 
+    @PostMapping("/Upload")
+    public Result csvUpload(@RequestPart("file") MultipartFile file){
+
+        File csv = new File(System.getProperty("java.io.tmpdir"));
+        if(!csv.mkdir()) return Result.fail("ERROR");
+        File tmp;
+        try {
+            tmp = File.createTempFile("tmp", ".csv");
+            file.transferTo(tmp);
+            List<Disaster> disasterList = ParseFileTools.parseFile(tmp, Disaster.class, false, ParseFileTools::parseCSV);
+            System.out.println(disasterList);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 }
