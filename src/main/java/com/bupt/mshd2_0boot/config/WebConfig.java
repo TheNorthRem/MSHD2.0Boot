d4aacf4859b7b943a11d05de51f1a0ab56e4105a
Jungle <1239946358@qq.com>
2023-12-18 18:14:02 +0800

    拦截器

--- src/main/java/com/bupt/mshd2_0boot/config/WebConfig.java
+++ src/main/java/com/bupt/mshd2_0boot/config/WebConfig.java
@@ -1,22 +1,36 @@
 package com.bupt.mshd2_0boot.config;
 
-import jakarta.annotation.Nonnull;
+import com.bupt.mshd2_0boot.utils.Interceptor.LoginInterceptor;
+import com.bupt.mshd2_0boot.utils.Interceptor.RefreshTokenInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
 
 /**
  * 跨域配置，下面的url就是前端对应的url
  */
 @Configuration
 public class WebConfig {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(@Nonnull CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+            public void addInterceptors(@NonNull InterceptorRegistry registry) {
+                // 登录拦截器 order 1 后执行
+                registry.addInterceptor(new LoginInterceptor()).
+                        excludePathPatterns("/login/**").
+                        order(1);
+                // 前置拦截器 order 0 先执行
+                registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).
+                        order(0);
             }
         };
     }
